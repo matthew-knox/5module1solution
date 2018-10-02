7feba50ad7356930562a28c5285f715e7a3ce390
Matthew Knox <mpknox2@illinois.edu>
2018-10-02 14:49:57 -0500

    done

---
+++ app.js
@@ -0,0 +1,45 @@
+(function () {
+  'use strict';
+
+  angular.module('LunchChecker', [])
+
+  .controller('LunchCheckerController', LunchCheckerController);
+
+  LunchCheckerController.$inject = ['$scope'];
+
+  function LunchCheckerController($scope) {
+    $scope.checkLunch = function () {
+      if(!$scope.lunchMeals || 0 === $scope.lunchMeals.length) {
+        $scope.customStyle = {
+          "color" : "red",
+        }
+        $scope.message = "Please enter data first";
+        return;
+      }
+
+      setLunchItemSize($scope);
+
+      if($scope.lunchItemSize > 3) {
+        $scope.customStyle = {
+          "color" : "red",
+        }
+        $scope.message = "Too much!";
+      }
+      else {
+        $scope.customStyle = {
+          "color" : "green",
+        }
+        $scope.message = "Enjoy!";
+      }
+    };
+
+    function setLunchItemSize($scope) {
+      var array = $scope.lunchMeals.split(',');
+      var lunchItemSize = 0;
+      for (var i = 0; i < array.length; i++) {
+        if (array[i] && 0 < array[i].length) lunchItemSize++;
+      }
+      $scope.lunchItemSize = lunchItemSize;
+    }
+  }
+})();
